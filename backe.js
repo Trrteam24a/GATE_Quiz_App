307e34fa140b7ff370a1e0c1a8dccffe6b7f1d2d
Deep Patil <118659788+Nakuyu@users.noreply.github.com>
2024-02-02 19:16:55 +0530

    backe1
    mongoDB sample connected and firebase authentication slightly configured

--- backe.js
+++ backe.js
@@ -1,17 +1,93 @@
 const express = require("express");
+const fs = require("fs");
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
+const admin = require("firebase-admin");
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config();
+//const cors = require("cors");
 
-const app = express(); // server create
+// const serviceAccount = require("path/to/keyofservice.json"); // no real key.json file here so wont work
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount)
+// });
+
+const app = express(); // create an Express app/server
 const port = 3000;
 
-// route definition
+const uri = 'mongodb+srv://parthkulkarni1602:XB8nFCAXYP8HCJQ5@cluster0.ldbgibz.mongodb.net/?retryWrites=true&w=majority';
+MongoClient.connect(process.env.uri);
+
+//const mongoClient = new MongoClient(uri, options);
+//connecting mongoDB
+MongoClient.connect(uri)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error.message);
+  });
+
+  //user schema
+  const userSchema = new mongoose.Schema({
+    username: String,
+    emailID: String,
+    Phone: String,
+
+  });
+  
+  const User = mongoose.model('User', userSchema);
+//route for routing requests to the express
 app.get("/", (req, res) => {
   console.log(req.url);
-  res.status(200).send('hmmmmmmmm123 - html temp home');
+  // Vue js async file
+  fs.readFile('signpage.vue', 'utf8', (err, data) => {
+    if (err) {
+      // Handle error
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+    res.set('Content-Type', 'text/html');
+    res.status(200).send(data);
+  });
 });
 
-// server listen
+const authenticateFirebase = async (req, res, next) => {
+  const idToken = req.header("AuthorizationofGoogle");
+
+  try {
+    const decodedToken = await admin.auth().verifyIdToken(idToken);
+    req.uid = decodedToken.uid;
+    return next();
+  } catch (error) {
+    return res.status(401).send("Unauthorized");
+  }
+};
+
+app.use(authenticateFirebase);
+
+app.post("signin", async (req, res) => {
+  //Firebase sign-in Google
+});
+
+app.post("signout", (req, res) => {
+  // Firebase sign-out
+});
+
+app.get("authorization", (req, res) => {
+  // authorization check
+});
+
+// Start Express server
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });
+
+//for closing mongodb
+// process.on('SIGINT', () => {
+//   client.close()
+//     .then(() => {
+//       console.log('MongoDB connection closed');
+//       process.exit(0);
+//     });
+// });
